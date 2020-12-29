c323e26c2c793095f85cd4f9a2ed513b71ad5a1d
esma17 <milkysf@gmail.com>
2020-12-29 12:21:22 -0600

    Added Utilities package and WebDriverFactory class

---
+++ src/test/java/com/nextBaseCRM/Utilities/WebDriverFactory.java
@@ -0,0 +1,28 @@
+package com.nextBaseCRM.Utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class WebDriverFactory {
+    public static WebDriver getDriver(String browserType) {
+        WebDriver driver = null;
+        if (browserType.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            return new ChromeDriver();
+
+        } else if (browserType.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            return new FirefoxDriver();
+        } else if (browserType.equalsIgnoreCase("safari")) {
+            return new SafariDriver();
+        } else {
+            System.out.println("Please enter a valid driver");
+            return null;
+        }
+
+
+    }
+}
--- src/test/java/com/nextBaseCRM/tests/esma/EsmaTest.java
+++ src/test/java/com/nextBaseCRM/tests/esma/EsmaTest.java
@@ -1,7 +1,10 @@
 package com.nextBaseCRM.tests.esma;
 
+import com.nextBaseCRM.Utilities.WebDriverFactory;
+
 public class EsmaTest {
     public static void main(String[] args) {
         System.out.println("Hello");
+        WebDriverFactory.getDriver("chrome");
     }
 }
