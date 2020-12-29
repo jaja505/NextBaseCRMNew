4d106784154ea5523d1f08800c756c5a3fab84a2
esma17 <milkysf@gmail.com>
2020-12-29 12:18:34 -0600

    Added dependencies

--- src/test/java/com/nextBaseCRM/tests/esma/testEsma.java
+++
@@ -1,8 +0,0 @@
-package com.nextBaseCRM.tests.esma;
-
-public class testEsma {
-    public static void main(String[] args) {
-        System.out.println("Esma");
-        System.out.println("hi");
-    }
-}
