9471ff9bce23b121e0f374c945262439a56a35a3
adnan <adnaninci86@gmail.com>
2020-12-29 13:40:38 -0600

    commit1

--- src/test/java/com/nextBaseCRM/tests/esma/EsmaTest.java
+++ src/test/java/com/nextBaseCRM/tests/esma/EsmaTest.java
@@ -6,5 +6,7 @@ public class EsmaTest {
     public static void main(String[] args) {
         System.out.println("Hello");
         WebDriverFactory.getDriver("chrome");
+
+        System.out.println("good job Esma!");
     }
 }
