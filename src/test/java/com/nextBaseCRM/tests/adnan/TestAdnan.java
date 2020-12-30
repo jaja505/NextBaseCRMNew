a0938412b284582392ccdcfabc293e256a9d8176
adnan <adnaninci86@gmail.com>
2020-12-29 18:47:49 -0600

    commit2

--- src/test/java/com/nextBaseCRM/tests/adnan/TestAdnan.java
+++ src/test/java/com/nextBaseCRM/tests/adnan/TestAdnan.java
@@ -1,7 +1,46 @@
 package com.nextBaseCRM.tests.adnan;
 
+import com.nextBaseCRM.Utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class TestAdnan {
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+
+        driver.manage().window().maximize();
+
+        driver.get("https://login2.nextbasecrm.com/stream/?apply_filter=Y&FIND=()");
+
+        WebElement username = driver.findElement(By.xpath("//input[@class='login-inp']"));
+
+        username.sendKeys("helpdesk45@cybertekschool.com");
+
+        WebElement password = driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
+
+        password.sendKeys("UserUser");
+
+        WebElement logIn = driver.findElement(By.xpath("//input[@value='Log In']"));
+
+        logIn.click();
+
+        WebElement searchBox = driver.findElement(By.xpath("//input[@id='search-textbox-input']"));
+
+        searchBox.sendKeys("inci" + Keys.ENTER);
+
+
+        String expectedSearch = "https://login2.nextbasecrm.com/stream/?apply_filter=Y&FIND=inci";
+
+        String actualSearch = driver.getCurrentUrl();
+
+        if (actualSearch.equalsIgnoreCase(expectedSearch))
+            System.out.println("test is passed");
+        else
+            System.err.println("try again later!!!");
+
+        driver.close();
     }
 }
