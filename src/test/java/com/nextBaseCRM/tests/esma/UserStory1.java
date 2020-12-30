431800ed54b92d156ad77ff909e55337360dc9c3
Jair Novello <jaja505@hotmail.com>
2020-12-29 22:39:06 -0500

    UserStory

--- src/test/java/com/nextBaseCRM/tests/esma/UserStory1.java
+++ src/test/java/com/nextBaseCRM/tests/esma/UserStory1.java
@@ -1,7 +1,23 @@
 package com.nextBaseCRM.tests.esma;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class UserStory1 {
     public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, 2);
+
+        driver.get("https://login2.nextbasecrm.com");
+
+        driver.findElement(By.name("USER_LOGIN")).sendKeys("helpdesk45@cybertekschool.com");
+        driver.findElement(By.name("USER_PASSWORD")).sendKeys("UserUser");
+        driver.findElement(By.className("login-btn")).click();
 
     }
 }
