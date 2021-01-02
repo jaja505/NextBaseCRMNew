2f5e4e3b5c602c29e18dd825b74ae67b8a6de9f5
Akita20 <jamila.osmn@gmail.com>
2021-01-01 19:05:32 -0800

    fixed the name of the firfox driver

--- src/test/java/com/nextBaseCRM/Utilities/AutomationTest.java
+++ src/test/java/com/nextBaseCRM/Utilities/AutomationTest.java
@@ -14,7 +14,7 @@ public class AutomationTest {public static WebDriver driverSetUp(String browserN
         return new ChromeDriver();
     }else if(browserName.equalsIgnoreCase("safari")){
         return new SafariDriver();
-    }else if(browserName.equalsIgnoreCase("fire fox")){
+    }else if(browserName.equalsIgnoreCase("firefox")){
         return new FirefoxDriver();
     }else if(browserName.equalsIgnoreCase("edge")){
         return new EdgeDriver();
