25c4ee8b4b3834cc8937023e990ff202a042cc76
Akita20 <jamila.osmn@gmail.com>
2021-01-22 20:26:33 -0800

    change the class name from User can login to Userstory1

--- src/test/java/com/nextBaseCRM/tests/jamila/userStory1/UserStory1.java
+++ src/test/java/com/nextBaseCRM/tests/jamila/userStory1/UserStory1.java
@@ -1,4 +1,4 @@
-package com.nextBaseCRM.tests.jamila;
+package com.nextBaseCRM.tests.jamila.userStory1;
 
 import com.nextBaseCRM.Utilities.AutomationTest;
 import com.nextBaseCRM.Utilities.WebDriverFactory;
